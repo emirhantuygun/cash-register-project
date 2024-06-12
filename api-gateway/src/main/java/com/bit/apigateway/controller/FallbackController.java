ab2818b4f451d2c9c08b26afc253123c2f878b19
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-12 23:49:30 +0300

    feat: fallback controller

---
+++ api-gateway/src/main/java/com/bit/apigateway/controller/FallbackController.java
@@ -0,0 +1,15 @@
+package com.bit.apigateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fallback")
+public class FallbackController {
+
+    @GetMapping("/auth")
+    public String fallbackAuth() {
+        return "Auth Service is temporarily unavailable. Please try again later.";
+    }
+}
