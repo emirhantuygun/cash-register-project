10b12ec79ba3289de4677c558302b06d5c71d0e0
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-16 02:01:08 +0300

    fix: FallbackControllerTest for api-gateway

--- api-gateway/src/test/java/com/bit/apigateway/FallbackControllerTest.java
+++ api-gateway/src/test/java/com/bit/apigateway/FallbackControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 @WebMvcTest(FallbackController.class)
 public class FallbackControllerTest {
 
