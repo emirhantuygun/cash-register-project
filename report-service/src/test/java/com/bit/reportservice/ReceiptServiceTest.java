db5d3040bbf70945f691c492c317245d271bbbd7
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-17 20:32:00 +0300

    fix: ReceiptServiceTest for report-service

--- report-service/src/test/java/com/bit/reportservice/ReceiptServiceTest.java
+++ report-service/src/test/java/com/bit/reportservice/ReceiptServiceTest.java
@@ -1,26 +1,53 @@
 package com.bit.reportservice;
 
+import com.bit.reportservice.dto.ProductResponse;
 import com.bit.reportservice.dto.SaleResponse;
 import com.bit.reportservice.exception.ReceiptGenerationException;
 import com.bit.reportservice.service.ReceiptService;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
 import java.io.ByteArrayOutputStream;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.*;
+
 
 @ExtendWith(MockitoExtension.class)
 public class ReceiptServiceTest {
 
     @InjectMocks
     private ReceiptService receiptService;
+
+//    @Test
+//    public void generateReceipt_ShouldReturnByteArray() throws ReceiptGenerationException {
+//        // Arrange
+//        ReflectionTestUtils.setField(receiptService, "FONT_PATH", "fonts/scoreboard.ttf");
+//        ReflectionTestUtils.setField(receiptService, "IMAGE_PATH", "/static/images/32bit.png");
+//        ReflectionTestUtils.setField(receiptService, "LOCATION", "some_location");
+//        ReflectionTestUtils.setField(receiptService, "CITY", "some_city");
+//        ReflectionTestUtils.setField(receiptService, "PHONE_NUMBER", "some_phone_number");
+//
+//        byte[] bytes = new byte[0];
+//
+//        ByteArrayOutputStream baos = mock();
+//        when(baos.toByteArray()).thenReturn(bytes);
+//
+//        List<ProductResponse> productResponses = List.of(new ProductResponse(1L, "Product", "Barcode", 1, BigDecimal.valueOf(1), BigDecimal.valueOf(1), 1L));
+//        assertTrue(bytes.getClass() == receiptService.generateReceipt(new SaleResponse(1L, "string", new Date(), "string", List.of("string"), productResponses, BigDecimal.ONE, BigDecimal.ONE, BigDecimal.ONE, BigDecimal.ONE, null)).getClass());
+//    }
 
     @Test
     public void generateReceipt_ShouldThrowReceiptGenerationException_WhenPdfWriterFails() {
