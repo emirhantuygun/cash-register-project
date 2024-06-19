f1d75bdd5047332fd3733592165e4b9b7b93ea9c
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-19 15:48:13 +0300

    add: UserManagementServiceSuite

---
+++ user-management-service/src/test/java/com/bit/usermanagementservice/UserManagementServiceSuite.java
@@ -0,0 +1,12 @@
+package com.bit.usermanagementservice;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({
+        "com.bit.usermanagementservice.controller",
+        "com.bit.usermanagementservice.service"
+})
+public class UserManagementServiceSuite {
+}
