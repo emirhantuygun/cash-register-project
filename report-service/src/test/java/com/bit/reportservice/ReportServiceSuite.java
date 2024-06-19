fb220060abf7d5a9a959cb7476dde6e6bc2cd051
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-19 15:44:54 +0300

    add: ReportServiceSuite

---
+++ report-service/src/test/java/com/bit/reportservice/ReportServiceSuite.java
@@ -0,0 +1,12 @@
+package com.bit.reportservice;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({
+        "com.bit.reportservice.controller",
+        "com.bit.reportservice.service"
+})
+public class ReportServiceSuite {
+}
