6f719bb295f7593c9807b71689c19961ab92de40
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-21 19:56:49 +0300

    fix: CacheServiceTest

--- product-service/src/test/java/com/bit/productservice/service/CacheServiceTest.java
+++ product-service/src/test/java/com/bit/productservice/service/CacheServiceTest.java
@@ -7,38 +7,24 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
-public class CacheServiceTest {
+class CacheServiceTest {
 
     @InjectMocks
     private CacheService cacheService;
 
-    @Mock
     private CacheManager cacheManager;
 
     @BeforeEach
     void setUp() {
         cacheManager = new ConcurrentMapCacheManager("product_id");
-    }
-
-    @Test
-    void givenValidProductResponse_whenCreateProductCache_thenProductResponseIsCached() {
-        // Arrange
-        ProductResponse productResponse = new ProductResponse();
-        productResponse.setId(1L);
-
-        // Act
-        ProductResponse result = cacheService.createProductCache(productResponse);
-
-        // Assert
-        assertNotNull(result);
-        assertEquals(productResponse, result);
-        assertTrue(cacheManager.getCache("product_id").get(1L).get() instanceof ProductResponse);
+        cacheService = new CacheService();
     }
 
     @Test
@@ -48,22 +34,11 @@ public class CacheServiceTest {
 
         // Assert
         assertNull(result);
-        assertNull(cacheManager.getCache("product_id").get(1L));
-    }
 
-    @Test
-    void givenValidProductResponse_whenUpdateProductCache_thenProductResponseIsUpdatedInCache() {
-        // Arrange
-        ProductResponse productResponse = new ProductResponse();
-        productResponse.setId(1L);
-
-        // Act
-        ProductResponse result = cacheService.updateProductCache(productResponse);
-
-        // Assert
-        assertNotNull(result);
-        assertEquals(productResponse, result);
-        assertTrue(cacheManager.getCache("product_id").get(1L).get() instanceof ProductResponse);
+        Cache cache = cacheManager.getCache("product_id");
+        assertNotNull(cache);
+        Cache.ValueWrapper valueWrapper = cache.get(1L);
+        assertNull(valueWrapper);
     }
 
     @Test
@@ -73,6 +48,10 @@ public class CacheServiceTest {
 
         // Assert
         assertNull(result);
-        assertNull(cacheManager.getCache("product_id").get(1L));
+
+        Cache cache = cacheManager.getCache("product_id");
+        assertNotNull(cache);
+        Cache.ValueWrapper valueWrapper = cache.get(1L);
+        assertNull(valueWrapper);
     }
 }
