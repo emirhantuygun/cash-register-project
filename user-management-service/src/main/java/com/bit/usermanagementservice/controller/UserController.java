5723c7cfab6d298cd103b0b59219e9adc5c81113
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-21 22:39:34 +0300

    add: log4j2.xml files

--- user-management-service/src/main/java/com/bit/usermanagementservice/controller/UserController.java
+++ user-management-service/src/main/java/com/bit/usermanagementservice/controller/UserController.java
@@ -12,8 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 import java.util.List;
 
 @Controller
