03568224b139d246ef2c7756a5e259e0a4dc09f4
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-21 17:26:26 +0300

    fix: serialization for redis

--- sale-service/src/main/java/com/bit/saleservice/dto/ProductResponse.java
+++ sale-service/src/main/java/com/bit/saleservice/dto/ProductResponse.java
@@ -15,9 +15,6 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class ProductResponse implements Serializable {
 
-    @Serial
-    private static final long serialVersionUID = 1L;
-
     private Long id;
     private String name;
     private String description;
