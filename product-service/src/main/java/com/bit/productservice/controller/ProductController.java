63794c08211ea4276134c0672f3f3134f36c0b68
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-22 00:40:17 +0300

    feat: logging for controller in product-service

--- product-service/src/main/java/com/bit/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/bit/productservice/controller/ProductController.java
@@ -8,6 +8,7 @@ import com.bit.productservice.service.ProductService;
 import com.bit.productservice.wrapper.ProductStockReturnRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.Page;
@@ -17,38 +18,47 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 import java.util.List;
 
+@Log4j2
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
 public class ProductController {
 
-    private static final Logger logger = LogManager.getLogger(ProductServiceApplication.class);
     private final ProductService productService;
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponse> getProduct(@PathVariable("id") Long id) {
-        logger.info("Received request to fetch product with ID: {}", id);
+        log.trace("Entering getProduct method in ProductController class");
+        log.info("Received request to fetch product with ID: {}", id);
+
         ProductResponse productResponse = productService.getProduct(id);
 
-        logger.info("Returning product response: {}", productResponse);
+        log.info("Returning product response: {}", productResponse);
+        log.trace("Exiting getProduct method in ProductController class");
         return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
 
     @GetMapping()
     public ResponseEntity<List<ProductResponse>> getAllProducts() {
-        logger.info("Received request to fetch all products");
+        log.trace("Entering getAllProducts method in ProductController class");
+        log.info("Received request to fetch all products");
+
         List<ProductResponse> productResponses = productService.getAllProducts();
 
-        logger.info("Returning {} product responses", productResponses.size());
+        log.info("Returning {} product responses", productResponses.size());
+        log.trace("Exiting getAllProducts method in ProductController class");
         return new ResponseEntity<>(productResponses, HttpStatus.OK);
     }
 
     @GetMapping("/deleted")
     public ResponseEntity<List<ProductResponse>> getDeletedProducts() {
-        logger.info("Received request to fetch all deleted products");
+        log.trace("Entering getDeletedProducts method in ProductController class");
+        log.info("Received request to fetch all deleted products");
+
         List<ProductResponse> deletedProductResponses = productService.getDeletedProducts();
 
-        logger.info("Returning {} deleted product responses", deletedProductResponses.size());
+        log.info("Returning {} deleted product responses", deletedProductResponses.size());
+        log.trace("Exiting getDeletedProducts method in ProductController class");
         return new ResponseEntity<>(deletedProductResponses, HttpStatus.OK);
     }
 
@@ -65,57 +75,82 @@ public class ProductController {
             @RequestParam(required = false) Integer minStock,
             @RequestParam(required = false) Integer maxStock
     ) {
+        log.trace("Entering getAllProductsFilteredAndSorted method in ProductController class");
 
         Page<ProductResponse> productResponses = productService.getAllProductsFilteredAndSorted(page, size, sortBy, direction, name, description, minPrice, maxPrice, minStock, maxStock);
 
+        log.info("Returning {} filtered and sorted product responses", productResponses.getTotalElements());
+        log.trace("Exiting getAllProductsFilteredAndSorted method in ProductController class");
         return new ResponseEntity<>(productResponses, HttpStatus.OK);
     }
 
     @PostMapping("/return")
     public ResponseEntity<String> returnProducts(@Valid @RequestBody ProductStockReturnRequest request) {
+        log.trace("Entering returnProducts method in ProductController class");
+
         productService.returnProducts(request);
+
+        log.info("Product return request processed successfully for request: {}", request);
+        log.trace("Exiting returnProducts method in ProductController class");
         return ResponseEntity.ok("Product return request processed successfully.");
     }
 
     @PostMapping
     public ResponseEntity<ProductResponse> createProduct(@RequestBody @Valid ProductRequest productRequest) throws AlgorithmNotFoundException {
-        logger.info("Received request to create product: {}", productRequest);
+        log.trace("Entering createProduct method in ProductController class");
+        log.info("Received request to create product: {}", productRequest);
+
         ProductResponse productResponse = productService.createProduct(productRequest);
 
-        logger.info("Returning product response's ID: {}", productResponse.getId());
+        log.info("Returning product response's ID: {}", productResponse.getId());
+        log.trace("Exiting createProduct method in ProductController class");
         return new ResponseEntity<>(productResponse, HttpStatus.CREATED);
     }
 
     @PutMapping("{id}")
     public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long id,
                                                          @RequestBody @Valid ProductRequest productRequest) throws AlgorithmNotFoundException {
-        logger.info("Received request to update product with ID {}: {}", id, productRequest);
+        log.trace("Entering updateProduct method in ProductController class");
+        log.info("Received request to update product with ID {}: {}", id, productRequest);
+
         ProductResponse productResponse = productService.updateProduct(id, productRequest);
 
-        logger.info("Returning product response with ID {}: {}", id, productResponse);
+        log.info("Returning product response with ID {}: {}", id, productResponse);
+        log.trace("Exiting updateProduct method in ProductController class");
         return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
 
     @PutMapping("/restore/{id}")
     public ResponseEntity<ProductResponse> restoreProduct(@PathVariable Long id) {
+        log.trace("Entering restoreProduct method in ProductController class");
+
         ProductResponse productResponse = productService.restoreProduct(id);
 
+        log.info("Returning restored product response with ID {}: {}", id, productResponse);
+        log.trace("Exiting restoreProduct method in ProductController class");
         return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
-        logger.info("Received request to delete product with ID: {}", id);
+        log.trace("Entering deleteProduct method in ProductController class");
+        log.info("Received request to delete product with ID: {}", id);
+
         productService.deleteProduct(id);
 
-        logger.info("Product with ID {} deleted successfully", id);
+        log.info("Product with ID {} deleted successfully", id);
+        log.trace("Exiting deleteProduct method in ProductController class");
         return new ResponseEntity<>("Product soft deleted successfully!", HttpStatus.OK);
     }
 
     @DeleteMapping("/permanent/{id}")
     public ResponseEntity<String> deleteProductPermanently(@PathVariable Long id) {
+        log.trace("Entering deleteProductPermanently method in ProductController class");
+
         productService.deleteProductPermanently(id);
 
+        log.info("Product with ID {} permanently deleted successfully", id);
+        log.trace("Exiting deleteProductPermanently method in ProductController class");
         return new ResponseEntity<>("Product deleted permanently!", HttpStatus.OK);
     }
 }
