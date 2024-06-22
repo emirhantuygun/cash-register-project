0971bbcafc5f6b915c2c5a05b820a8787d853284
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-22 15:16:39 +0300

    add: logging for service in sale service

--- sale-service/src/main/java/com/bit/saleservice/controller/CampaignController.java
+++ sale-service/src/main/java/com/bit/saleservice/controller/CampaignController.java
@@ -25,15 +25,23 @@ public class CampaignController {
 
     @GetMapping("/{id}")
     public ResponseEntity<CampaignResponse> getCampaign(@PathVariable("id") Long id) {
-        CampaignResponse campaignResponse = campaignService.getCampaign(id);
+        log.trace("Entering getCampaign method in CampaignController with id: {}", id);
 
+        CampaignResponse campaignResponse = campaignService.getCampaign(id);
+        log.info("Successfully retrieved campaign with id: {}", id);
+
+        log.trace("Exiting getCampaign method in CampaignController with id: {}", id);
         return new ResponseEntity<>(campaignResponse, HttpStatus.OK);
     }
 
     @GetMapping()
     public ResponseEntity<List<CampaignResponse>> getAllCampaigns() {
-        List<CampaignResponse> campaignResponses = campaignService.getAllCampaigns();
+        log.trace("Entering getAllCampaigns method in CampaignController");
 
+        List<CampaignResponse> campaignResponses = campaignService.getAllCampaigns();
+        log.info("Successfully retrieved all campaigns");
+
+        log.trace("Exiting getAllCampaigns method in CampaignController");
         return new ResponseEntity<>(campaignResponses, HttpStatus.OK);
     }
 
@@ -47,8 +55,14 @@ public class CampaignController {
             @RequestParam(required = false) String details,
             @RequestParam(required = false) Boolean isExpired
     ) {
-        Page<CampaignResponse> campaignResponses = campaignService.getAllCampaignsFilteredAndSorted(page, size, sortBy, direction, name, details, isExpired);
+        log.trace("Entering getAllCampaignsFilteredAndSorted method in CampaignController " +
+                        "with page: {}, size: {}, sortBy: {}, direction: {}, name: {}, details: {}, isExpired: {}",
+                page, size, sortBy, direction, name, details, isExpired);
 
+        Page<CampaignResponse> campaignResponses = campaignService.getAllCampaignsFilteredAndSorted(page, size, sortBy, direction, name, details, isExpired);
+        log.info("Successfully retrieved filtered and sorted campaigns");
+
+        log.trace("Exiting getAllCampaignsFilteredAndSorted method in CampaignController");
         return new ResponseEntity<>(campaignResponses, HttpStatus.OK);
     }
 }
--- sale-service/src/main/java/com/bit/saleservice/controller/SaleController.java
+++ sale-service/src/main/java/com/bit/saleservice/controller/SaleController.java
@@ -1,55 +1,59 @@
 package com.bit.saleservice.controller;
 
-import com.bit.saleservice.SaleServiceApplication;
 import com.bit.saleservice.dto.SaleRequest;
 import com.bit.saleservice.dto.SaleResponse;
 import com.bit.saleservice.exception.HeaderProcessingException;
 import com.bit.saleservice.service.SaleService;
 import com.bit.saleservice.wrapper.PageWrapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.data.domain.Page;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import jakarta.validation.Valid;
+
 import java.math.BigDecimal;
 import java.util.List;
 
+@Log4j2
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/sales")
 public class SaleController {
 
-    private final Logger logger = LogManager.getLogger(SaleServiceApplication.class);
     private final SaleService saleService;
 
     @GetMapping("/{id}")
     public ResponseEntity<SaleResponse> getSale(@PathVariable("id") Long id) {
-        logger.info("Received request to fetch sale with ID: {}", id);
-        SaleResponse saleResponse = saleService.getSale(id);
+        log.trace("Entering getSale method in SaleController with id: {}", id);
 
-        logger.info("Returning sale response: {}", saleResponse);
+        SaleResponse saleResponse = saleService.getSale(id);
+        log.info("Successfully retrieved sale with id: {}", id);
+
+        log.trace("Exiting getSale method in SaleController with id: {}", id);
         return new ResponseEntity<>(saleResponse, HttpStatus.OK);
     }
 
     @GetMapping()
     public ResponseEntity<List<SaleResponse>> getAllSales() {
-        logger.info("Received request to fetch all sales");
-        List<SaleResponse> saleResponses = saleService.getAllSales();
+        log.trace("Entering getAllSales method in SaleController");
 
-        logger.info("Returning {} sale responses", saleResponses.size());
+        List<SaleResponse> saleResponses = saleService.getAllSales();
+        log.info("Successfully retrieved all sales");
+
+        log.trace("Exiting getAllSales method in SaleController");
         return new ResponseEntity<>(saleResponses, HttpStatus.OK);
     }
 
     @GetMapping("/deleted")
     public ResponseEntity<List<SaleResponse>> getDeletedSales() {
-        logger.info("Received request to fetch all deleted sales");
-        List<SaleResponse> deletedSaleResponses = saleService.getDeletedSales();
+        log.trace("Entering getDeletedSales method in SaleController");
 
-        logger.info("Returning {} deleted sale responses", deletedSaleResponses.size());
+        List<SaleResponse> deletedSaleResponses = saleService.getDeletedSales();
+        log.info("Successfully retrieved deleted sales");
+
+        log.trace("Exiting getDeletedSales method in SaleController");
         return new ResponseEntity<>(deletedSaleResponses, HttpStatus.OK);
     }
 
@@ -66,67 +70,80 @@ public class SaleController {
             @RequestParam(required = false) String startDate,
             @RequestParam(required = false) String endDate
     ) {
-        logger.info("Received request to fetch all sales with filters and sorting: page={}, size={}, sortBy={}, direction={}, cashier={}, paymentMethod={}, minPrice={}, maxPrice={}, startDate={}, endDate={}",
+        log.trace("Entering getAllSalesFilteredAndSorted method in SaleController with page: {}, size: {}, sortBy: {}, direction: {}, cashier: {}, paymentMethod: {}, minTotal: {}, maxTotal: {}, startDate: {}, endDate: {}",
                 page, size, sortBy, direction, cashier, paymentMethod, minTotal, maxTotal, startDate, endDate);
-        Page<SaleResponse> saleResponses = saleService.getAllSalesFilteredAndSorted(page, size, sortBy, direction, cashier, paymentMethod, minTotal, maxTotal, startDate, endDate);
 
-        logger.info("Returning {} sale responses filtered and sorted", saleResponses.getTotalElements());
-        PageWrapper<SaleResponse> response = new PageWrapper<>();
-        response.setContent(saleResponses.getContent());
-        response.setPageNumber(page);
-        response.setPageSize(size);
-        response.setTotalElements(saleResponses.getTotalElements());
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        PageWrapper<SaleResponse> saleResponsePageWrapper = saleService.getAllSalesFilteredAndSorted(page, size, sortBy, direction, cashier, paymentMethod, minTotal, maxTotal, startDate, endDate);
+        log.info("Successfully retrieved filtered and sorted sales");
+
+        log.trace("Exiting getAllSalesFilteredAndSorted method in SaleController");
+        return new ResponseEntity<>(saleResponsePageWrapper, HttpStatus.OK);
     }
 
     @PostMapping()
     public ResponseEntity<SaleResponse> createSale(@RequestBody SaleRequest saleRequest) throws HeaderProcessingException {
-        logger.info("Received request to create sale: {}", saleRequest);
-         SaleResponse saleResponse = saleService.createSale(saleRequest);
+        log.trace("Entering createSale method in SaleController with saleRequest: {}", saleRequest);
 
-        logger.info("Returning sale response's ID: {}", saleResponse.getId());
+        SaleResponse saleResponse = saleService.createSale(saleRequest);
+        log.info("Successfully created sale");
+
+        log.trace("Exiting createSale method in SaleController");
         return new ResponseEntity<>(saleResponse, HttpStatus.CREATED);
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<SaleResponse> updateSale(@PathVariable Long id,
-                                                         @RequestBody @Valid SaleRequest saleRequest) throws HeaderProcessingException {
-        logger.info("Received request to update sale with ID {}: {}", id, saleRequest);
-        SaleResponse saleResponse = saleService.updateSale(id, saleRequest);
+    public ResponseEntity<SaleResponse> updateSale(@PathVariable Long id, @RequestBody @Valid SaleRequest saleRequest) throws HeaderProcessingException {
+        log.trace("Entering updateSale method in SaleController with id: {} and saleRequest: {}", id, saleRequest);
 
-        logger.info("Returning sale response with ID {}: {}", id, saleResponse);
+        SaleResponse saleResponse = saleService.updateSale(id, saleRequest);
+        log.info("Successfully updated sale with id: {}", id);
+
+        log.trace("Exiting updateSale method in SaleController with id: {}", id);
         return new ResponseEntity<>(saleResponse, HttpStatus.OK);
     }
 
     @PutMapping("/cancel/{id}")
-    public ResponseEntity<String> cancelSale(@PathVariable Long id){
-        saleService.cancelSale(id);
+    public ResponseEntity<String> cancelSale(@PathVariable Long id) {
+        log.trace("Entering cancelSale method in SaleController with id: {}", id);
 
+        saleService.cancelSale(id);
+        log.info("Successfully cancelled sale with id: {}", id);
+
+        log.trace("Exiting cancelSale method in SaleController with id: {}", id);
         return new ResponseEntity<>("Sale cancelled successfully!", HttpStatus.OK);
     }
 
     @PutMapping("/restore/{id}")
-    public ResponseEntity<SaleResponse> restoreSale(@PathVariable Long id){
-        SaleResponse saleResponse = saleService.restoreSale(id);
+    public ResponseEntity<SaleResponse> restoreSale(@PathVariable Long id) {
+        log.trace("Entering restoreSale method in SaleController with id: {}", id);
 
+        SaleResponse saleResponse = saleService.restoreSale(id);
+        log.info("Successfully restored sale with id: {}", id);
+
+        log.trace("Exiting restoreSale method in SaleController with id: {}", id);
         return new ResponseEntity<>(saleResponse, HttpStatus.OK);
     }
 
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteSale(@PathVariable Long id){
-        logger.info("Received request to delete sale with ID: {}", id);
-        saleService.deleteSale(id);
+    public ResponseEntity<String> deleteSale(@PathVariable Long id) {
+        log.trace("Entering deleteSale method in SaleController with id: {}", id);
 
-        logger.info("Sale with ID {} soft deleted successfully", id);
+        saleService.deleteSale(id);
+        log.info("Successfully soft deleted sale with id: {}", id);
+
+        log.trace("Exiting deleteSale method in SaleController with id: {}", id);
         return new ResponseEntity<>("Sale soft deleted successfully!", HttpStatus.OK);
     }
 
     @DeleteMapping("/permanent/{id}")
-    public ResponseEntity<String> deleteSalePermanently(@PathVariable Long id){
-        saleService.deleteSalePermanently(id);
+    public ResponseEntity<String> deleteSalePermanently(@PathVariable Long id) {
+        log.trace("Entering deleteSalePermanently method in SaleController with id: {}", id);
 
+        saleService.deleteSalePermanently(id);
+        log.info("Successfully permanently deleted sale with id: {}", id);
+
+        log.trace("Exiting deleteSalePermanently method in SaleController with id: {}", id);
         return new ResponseEntity<>("Sale deleted permanently!", HttpStatus.OK);
     }
-
 }
