43f121e96b4285a66e4763f7032dcbba46ef374a
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-22 18:37:21 +0300

    add: logging for controller in user-service

--- user-management-service/src/main/java/com/bit/usermanagementservice/controller/UserController.java
+++ user-management-service/src/main/java/com/bit/usermanagementservice/controller/UserController.java
@@ -1,50 +1,57 @@
 package com.bit.usermanagementservice.controller;
 
-import com.bit.usermanagementservice.UserManagementServiceApplication;
 import com.bit.usermanagementservice.dto.UserRequest;
 import com.bit.usermanagementservice.dto.UserResponse;
 import com.bit.usermanagementservice.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import jakarta.validation.Valid;
+
 import java.util.List;
 
+@Log4j2
 @Controller
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
-    private static final Logger logger = LogManager.getLogger(UserManagementServiceApplication.class);
-
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable("id") Long id) {
+        log.trace("Entering getUser method in UserController with id: {}", id);
 
         UserResponse userResponse = userService.getUser(id);
+        log.info("Retrieved user details for id: {}", id);
 
+        log.trace("Exiting getUser method in UserController");
         return new ResponseEntity<>(userResponse, HttpStatus.OK);
     }
 
     @GetMapping()
     public ResponseEntity<List<UserResponse>> getAllUsers() {
+        log.trace("Entering getAllUsers method in UserController");
 
         List<UserResponse> userResponses = userService.getAllUsers();
+        log.info("Retrieved all users");
 
+        log.trace("Exiting getAllUsers method in UserController");
         return new ResponseEntity<>(userResponses, HttpStatus.OK);
     }
 
     @GetMapping("/deleted")
     public ResponseEntity<List<UserResponse>> getDeletedUsers() {
+        log.trace("Entering getDeletedUsers method in UserController");
 
         List<UserResponse> deletedUserResponses = userService.getDeletedUsers();
+        log.info("Retrieved deleted users");
 
+        log.trace("Exiting getDeletedUsers method in UserController");
         return new ResponseEntity<>(deletedUserResponses, HttpStatus.OK);
     }
 
@@ -59,49 +66,69 @@ public class UserController {
             @RequestParam(required = false) String email,
             @RequestParam(required = false) String roleName
     ) {
-        logger.info("Received request to fetch all users with filters and sorting: page={}, size={}, sortBy={}, direction={}, name={}, username={}, email={}",
-                page, size, sortBy, direction, name, username, email);
-        Page<UserResponse> users = userService.getAllUsersFilteredAndSorted(page, size, sortBy, direction, name, username, email, roleName);
+        log.trace("Entering getAllUsersFilteredAndSorted method in UserController with page: {}, size: {}, sortBy: {}, direction: {}, name: {}, username: {}, email: {}, roleName: {}",
+                page, size, sortBy, direction, name, username, email, roleName);
 
-        logger.info("Returning {} user responses filtered and sorted", users.getTotalElements());
+        Page<UserResponse> users = userService.getAllUsersFilteredAndSorted(page, size, sortBy, direction, name, username, email, roleName);
+        log.info("Retrieved filtered and sorted users");
+
+        log.trace("Exiting getAllUsersFilteredAndSorted method in UserController");
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     @PostMapping()
     public ResponseEntity<UserResponse> createUser(@RequestBody @Valid UserRequest userRequest) {
+        log.trace("Entering createUser method in UserController with userRequest: {}", userRequest);
 
         UserResponse userResponse = userService.createUser(userRequest);
+        log.info("Created new user with details: {}", userResponse);
 
+        log.trace("Exiting createUser method in UserController");
         return new ResponseEntity<>(userResponse, HttpStatus.CREATED);
     }
 
     @PutMapping("{id}")
     public ResponseEntity<UserResponse> updateUser(@PathVariable Long id,
                                                          @RequestBody @Valid UserRequest userRequest){
-        UserResponse userResponse = userService.updateUser(id, userRequest);
+        log.trace("Entering updateUser method in UserController with id: {}, userRequest: {}", id, userRequest);
 
+        UserResponse userResponse = userService.updateUser(id, userRequest);
+        log.info("Updated user with id: {}", id);
+
+        log.trace("Exiting updateUser method in UserController");
         return new ResponseEntity<>(userResponse, HttpStatus.OK);
     }
 
     @PutMapping("/restore/{id}")
     public ResponseEntity<UserResponse> restoreUser(@PathVariable Long id){
-        UserResponse userResponse = userService.restoreUser(id);
+        log.trace("Entering restoreUser method in UserController with id: {}", id);
 
+        UserResponse userResponse = userService.restoreUser(id);
+        log.info("Restored user with id: {}", id);
+
+        log.trace("Exiting restoreUser method in UserController");
         return new ResponseEntity<>(userResponse, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable Long id){
-        userService.deleteUser(id);
+        log.trace("Entering deleteUser method in UserController with id: {}", id);
 
+        userService.deleteUser(id);
+        log.info("Soft deleted user with id: {}", id);
+
+        log.trace("Exiting deleteUser method in UserController");
         return new ResponseEntity<>("User soft deleted successfully!", HttpStatus.OK);
     }
 
     @DeleteMapping("/permanent/{id}")
     public ResponseEntity<String> deleteUserPermanently(@PathVariable Long id){
-        userService.deleteUserPermanently(id);
+        log.trace("Entering deleteUserPermanently method in UserController with id: {}", id);
 
+        userService.deleteUserPermanently(id);
+        log.info("Permanently deleted user with id: {}", id);
+
+        log.trace("Exiting deleteUserPermanently method in UserController");
         return new ResponseEntity<>("User deleted permanently!", HttpStatus.OK);
     }
-
 }
