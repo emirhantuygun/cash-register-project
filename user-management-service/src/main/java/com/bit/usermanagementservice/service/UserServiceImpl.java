8a73c6c8a8858adef4882bffc2eaa24c4985f7df
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-22 22:22:41 +0300

    add: logging for service in user-service

--- user-management-service/src/main/java/com/bit/usermanagementservice/service/UserServiceImpl.java
+++ user-management-service/src/main/java/com/bit/usermanagementservice/service/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package com.bit.usermanagementservice.service;
 
-import com.bit.usermanagementservice.UserManagementServiceApplication;
 import com.bit.usermanagementservice.annotation.ExcludeFromGeneratedCoverage;
 import com.bit.usermanagementservice.dto.AuthUserRequest;
 import com.bit.usermanagementservice.dto.UserRequest;
@@ -17,8 +16,7 @@ import com.bit.usermanagementservice.wrapper.UpdateUserMessage;
 import io.micrometer.common.util.StringUtils;
 import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -33,6 +31,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -40,7 +39,7 @@ public class UserServiceImpl implements UserService {
 
     @Value("#{'${default-roles}'.split(', ')}")
     private final List<String> DEFAULT_ROLES;
-    private static final Logger logger = LogManager.getLogger(UserManagementServiceApplication.class);
+
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final RabbitTemplate rabbitTemplate;
@@ -66,34 +65,46 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserResponse getUser(Long id) {
-        AppUser user = userRepository.findById(id)
-                .orElseThrow(() -> new UserNotFoundException("User not found with id " + id));
+        log.trace("Entering getUser method in UserServiceImpl with id: {}", id);
 
-        logger.info("User retrieved");
+        AppUser user = userRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.error("User not found with id: {}", id);
+                    return new UserNotFoundException("User not found with id " + id);
+                });
+        log.info("User found with id: {}", id);
+
+        log.trace("Exiting getUser method in UserServiceImpl");
         return mapToUserResponse(user);
     }
 
     @Override
     public List<UserResponse> getAllUsers() {
-        logger.info("Fetching all users");
-        List<AppUser> users = userRepository.findAll();
+        log.trace("Entering getAllUsers method in UserServiceImpl");
 
-        logger.info("Retrieved {} users", users.size());
+        List<AppUser> users = userRepository.findAll();
+        log.info("Retrieved {} users", users.size());
+
+        log.trace("Exiting getAllUsers method in UserServiceImpl");
         return users.stream().map(this::mapToUserResponse).toList();
     }
 
     @Override
     public List<UserResponse> getDeletedUsers() {
-        logger.info("Fetching all deleted users");
-        List<AppUser> users = userRepository.findSoftDeletedUsers();
+        log.trace("Entering getDeletedUsers method in UserServiceImpl");
 
-        logger.info("Retrieved {} deleted users", users.size());
+        List<AppUser> users = userRepository.findSoftDeletedUsers();
+        log.info("Retrieved {} deleted users", users.size());
+
+        log.trace("Exiting getDeletedUsers method in UserServiceImpl");
         return users.stream().map(this::mapToUserResponse).toList();
     }
 
     @Override
     public Page<UserResponse> getAllUsersFilteredAndSorted(int page, int size, String sortBy, String direction, String name, String username, String email, String roleName) {
-        logger.info("Fetching all users with filters and sorting");
+        log.trace("Entering getAllUsersFilteredAndSorted method in UserServiceImpl with parameters: page={}, size={}, sortBy={}, direction={}, name={}, username={}, email={}, roleName={}",
+                page, size, sortBy, direction, name, username, email, roleName);
+
         Pageable pageable = PageRequest.of(page, size, Sort.Direction.valueOf(direction.toUpperCase()), sortBy);
 
         Page<AppUser> usersPage = userRepository.findAll((root, query, criteriaBuilder) -> {
@@ -101,16 +112,16 @@ public class UserServiceImpl implements UserService {
             List<Predicate> predicates = getPredicates(name, username, email, roleName, root, criteriaBuilder);
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         }, pageable);
+        log.info("Retrieved {} users", usersPage.getTotalElements());
 
-        logger.info("Retrieved {} users", usersPage.getTotalElements());
+        log.trace("Exiting getAllUsersFilteredAndSorted method in UserServiceImpl");
         return usersPage.map(this::mapToUserResponse);
     }
-
 
     @Override
     @Transactional
     public UserResponse createUser(UserRequest userRequest) {
-        logger.info("Creating a user");
+        log.trace("Entering createUser method in UserServiceImpl with userRequest: {}", userRequest);
 
         validateRoles(userRequest.getRoles());
         checkUniqueness(userRequest);
@@ -127,13 +138,17 @@ public class UserServiceImpl implements UserService {
         try {
             AuthUserRequest authUserRequest = mapToAuthUserRequest(userRequest);
             rabbitTemplate.convertAndSend(EXCHANGE, ROUTING_KEY_CREATE, authUserRequest);
+            log.info("Sent create message to RabbitMQ for user: {}", userRequest.getUsername());
+
         } catch (Exception e) {
+            log.error("Failed to send create message to RabbitMQ for user: {}", userRequest.getUsername(), e);
             throw new RabbitMQException("Failed to send create message to RabbitMQ", e);
         }
 
         userRepository.save(user);
+        log.info("User created with username: {}", userRequest.getUsername());
 
-        logger.info("Created user with ID: {}", user.getId());
+        log.trace("Exiting createUser method in UserServiceImpl");
         return mapToUserResponseWithoutPassword(user);
     }
 
@@ -141,12 +156,15 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public UserResponse updateUser(Long id, UserRequest userRequest) {
-        logger.info("Updating user with ID {}: {}", id, userRequest);
+        log.trace("Entering updateUser method in UserServiceImpl with id: {} and userRequest: {}", id, userRequest);
 
         validateRoles(userRequest.getRoles());
 
         AppUser existingUser = userRepository.findById(id)
-                .orElseThrow(() -> new UserNotFoundException("User doesn't exist with id " + id));
+                .orElseThrow(() -> {
+                    log.error("User does not exist with id " + id);
+                    return new UserNotFoundException("User doesn't exist with id " + id);
+                });
 
         checkUniquenessForUpdate(existingUser, userRequest);
 
@@ -160,72 +178,106 @@ public class UserServiceImpl implements UserService {
             AuthUserRequest authUserRequest = mapToAuthUserRequest(userRequest);
             UpdateUserMessage updateUserMessage = new UpdateUserMessage(id, authUserRequest);
             rabbitTemplate.convertAndSend(EXCHANGE, ROUTING_KEY_UPDATE, updateUserMessage);
+            log.info("Sent update message to RabbitMQ for user: {}", userRequest.getUsername());
+
         } catch (Exception e) {
+            log.error("Failed to send update message to RabbitMQ for user: {}", userRequest.getUsername(), e);
             throw new RabbitMQException("Failed to send update message to RabbitMQ", e);
         }
 
         userRepository.save(existingUser);
+        log.info("User updated with id: {}", id);
 
-        logger.info("User updated with id {}", id);
+        log.trace("Exiting updateUser method in UserServiceImpl");
         return mapToUserResponseWithoutPassword(existingUser);
     }
 
     @Override
     @Transactional
     public UserResponse restoreUser(Long id) {
-        if(!userRepository.existsByIdAndDeletedTrue(id))
+        log.trace("Entering restoreUser method in UserServiceImpl with id: {}", id);
+
+        if (!userRepository.existsByIdAndDeletedTrue(id)) {
+            log.warn("User with id {} is not soft-deleted and cannot be restored.", id);
             throw new UserNotSoftDeletedException("User with id " + id + " is not soft-deleted and cannot be restored.");
+        }
 
         try {
             rabbitTemplate.convertAndSend(EXCHANGE, ROUTING_KEY_RESTORE, id);
+            log.info("Sent restore message to RabbitMQ for user with id: {}", id);
+
         } catch (Exception e) {
+            log.error("Failed to send restore message to RabbitMQ for user with id: {}", id, e);
             throw new RabbitMQException("Failed to send restore message to RabbitMQ", e);
         }
 
         userRepository.restoreUser(id);
         AppUser user = userRepository.findById(id)
-                .orElseThrow(() -> new UserNotFoundException("Couldn't restore the user with id " + id));
+                .orElseThrow(() -> {
+                    log.error("Couldn't restore the user with id " + id);
+                    return new UserNotFoundException("Couldn't restore the user with id " + id);
+                });
+        log.info("User restored with id: {}", id);
 
+        log.trace("Exiting restoreUser method in UserServiceImpl");
         return mapToUserResponseWithoutPassword(user);
     }
 
     @Override
     @Transactional
     public void deleteUser(Long id) {
-        logger.info("Deleting user with ID: {}", id);
+        log.trace("Entering deleteUser method in UserServiceImpl with id: {}", id);
 
-        if(!userRepository.existsById(id))
-           throw new UserNotFoundException("User not found with id " + id);
+        if (!userRepository.existsById(id)) {
+            log.warn("User not found with id: {}", id);
+            throw new UserNotFoundException("User not found with id " + id);
+        }
 
         try {
             rabbitTemplate.convertAndSend(EXCHANGE, ROUTING_KEY_DELETE, id);
+            log.info("Sent delete message to RabbitMQ for user with id: {}", id);
+
         } catch (Exception e) {
+            log.error("Failed to send delete message to RabbitMQ for user with id: {}", id, e);
             throw new RabbitMQException("Failed to send delete message to RabbitMQ", e);
         }
 
         userRepository.deleteById(id);
+        log.info("User soft deleted with id: {}", id);
 
-        logger.info("User soft-deleted");
+        log.trace("Exiting deleteUser method in UserServiceImpl");
     }
 
     @Override
     @Transactional
     public void deleteUserPermanently(Long id) {
-        if(!userRepository.existsById(id))
+        log.trace("Entering deleteUserPermanently method in UserServiceImpl with id: {}", id);
+
+        if (!userRepository.existsById(id)) {
+            log.warn("User not found with id: {}", id);
             throw new UserNotFoundException("User not found with id " + id);
+        }
 
         try {
             rabbitTemplate.convertAndSend(EXCHANGE, ROUTING_KEY_DELETE_PERMANENT, id);
+            log.info("Sent delete-permanent message to RabbitMQ for user with id: {}", id);
+
         } catch (Exception e) {
+            log.error("Failed to send delete-permanent message to RabbitMQ for user with id: {}", id, e);
             throw new RabbitMQException("Failed to send delete-permanent message to RabbitMQ", e);
         }
 
         userRepository.deleteRolesForUser(id);
         userRepository.deletePermanently(id);
+        log.info("User permanently deleted with id: {}", id);
+
+        log.trace("Exiting deleteUserPermanently method in UserServiceImpl");
     }
 
     @ExcludeFromGeneratedCoverage
-    private List<Predicate> getPredicates (String name, String username, String email, String roleName, Root<AppUser> root, CriteriaBuilder criteriaBuilder){
+    private List<Predicate> getPredicates(String name, String username, String email, String roleName, Root<AppUser> root, CriteriaBuilder criteriaBuilder) {
+        log.trace("Entering getPredicates method in UserServiceImpl with parameters: name={}, username={}, email={}, roleName={}", name, username, email, roleName);
+
         List<Predicate> predicates = new ArrayList<>();
         if (StringUtils.isNotBlank(name)) {
             predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), "%" + name.toLowerCase() + "%"));
@@ -240,43 +292,73 @@ public class UserServiceImpl implements UserService {
             Join<AppUser, Role> roleJoin = root.join("roles", JoinType.INNER);
             predicates.add(criteriaBuilder.equal(criteriaBuilder.lower(roleJoin.get("roleName")), roleName.toLowerCase()));
         }
+        log.info("Created {} predicates for filtering", predicates.size());
+
+        log.trace("Exiting getPredicates method in UserServiceImpl");
         return predicates;
     }
 
     @ExcludeFromGeneratedCoverage
     private void checkUniqueness(UserRequest userRequest) {
-        if (userRepository.existsByUsername(userRequest.getUsername()) || userRepository.existsByEmail(userRequest.getEmail())) {
-            throw new IllegalArgumentException("Username or email already in use");
+        log.trace("Entering checkUniqueness method in UserServiceImpl with userRequest: {}", userRequest);
+
+        if (userRepository.existsByUsername(userRequest.getUsername())) {
+            log.warn("Username {} is already taken", userRequest.getUsername());
+            throw new IllegalArgumentException("Username is already taken: " + userRequest.getUsername());
         }
+        if (userRepository.existsByEmail(userRequest.getEmail())) {
+            log.warn("Email {} is already taken", userRequest.getEmail());
+            throw new IllegalArgumentException("Email is already taken: " + userRequest.getEmail());
+        }
+
+        log.trace("Exiting checkUniqueness method in UserServiceImpl");
     }
 
     @ExcludeFromGeneratedCoverage
-    private void checkUniquenessForUpdate(AppUser appUser, UserRequest userRequest){
+    private void checkUniquenessForUpdate(AppUser appUser, UserRequest userRequest) {
+        log.trace("Entering checkUniquenessForUpdate method in UserServiceImpl with existingUser: {} and userRequest: {}", appUser, userRequest);
+
         if (!userRequest.getUsername().equals(appUser.getUsername()) && userRepository.existsByUsername(userRequest.getUsername())) {
+            log.warn("Username {} is already taken by another user", userRequest.getUsername());
             throw new DataIntegrityViolationException("Username already exists: " + userRequest.getUsername());
         }
 
         if (!userRequest.getEmail().equals(appUser.getEmail()) && userRepository.existsByEmail(userRequest.getEmail())) {
+            log.warn("Email {} is already taken by another user", userRequest.getEmail());
             throw new DataIntegrityViolationException("Email already exists: " + userRequest.getEmail());
         }
+
+        log.trace("Exiting checkUniquenessForUpdate method in UserServiceImpl");
     }
 
     @ExcludeFromGeneratedCoverage
     protected void validateRoles(List<String> roles) {
+        log.trace("Entering validateRoles method in UserServiceImpl with roleNames");
 
         for (String role : roles) {
             if (!DEFAULT_ROLES.contains(role)) {
+                log.error("Invalid role: " + role);
                 throw new InvalidRoleException("Invalid role: " + role);
             }
         }
+
+        log.trace("Exiting validateRoles method in UserServiceImpl");
     }
-    private AuthUserRequest mapToAuthUserRequest (UserRequest userRequest) {
+
+    private AuthUserRequest mapToAuthUserRequest(UserRequest userRequest) {
+        log.trace("Entering mapToAuthUserRequest method in UserServiceImpl with userRequest: {}", userRequest);
+
+        log.trace("Exiting mapToAuthUserRequest method in UserServiceImpl");
         return new AuthUserRequest(
                 userRequest.getUsername(),
                 userRequest.getPassword(),
                 userRequest.getRoles());
     }
+
     private UserResponse mapToUserResponse(AppUser user) {
+        log.trace("Entering mapToUserResponse method in UserServiceImpl with user: {}", user);
+
+        log.trace("Exiting mapToUserResponse method in UserServiceImpl");
         return UserResponse.builder()
                 .id(user.getId())
                 .name(user.getName())
@@ -286,7 +368,11 @@ public class UserServiceImpl implements UserService {
                 .roles(getRolesAsString(user.getRoles()))
                 .build();
     }
+
     private UserResponse mapToUserResponseWithoutPassword(AppUser user) {
+        log.trace("Entering mapToUserResponseWithoutPassword method in UserServiceImpl with user: {}", user);
+
+        log.trace("Exiting mapToUserResponseWithoutPassword method in UserServiceImpl");
         return UserResponse.builder()
                 .id(user.getId())
                 .name(user.getName())
@@ -295,21 +381,30 @@ public class UserServiceImpl implements UserService {
                 .roles(getRolesAsString(user.getRoles()))
                 .build();
     }
+
     private List<String> getRolesAsString(List<Role> roles) {
+        log.trace("Entering getRolesAsString method in UserServiceImpl with roleNames");
+
+        log.trace("Exiting getRolesAsString method in UserServiceImpl with roleNames");
         return roles.stream()
                 .map(Role::getRoleName)
                 .collect(Collectors.toList());
     }
+
     private List<Role> getRolesAsRole(List<String> roles) {
+        log.trace("Entering getRolesAsRole method in UserServiceImpl with roleNames");
+
         List<Role> rolesList = new ArrayList<>();
         roles.forEach(roleName -> {
             Role role = roleRepository.findByRoleName(roleName)
                     .orElseThrow(() -> {
-                        logger.error("Role {} not found", roleName);
+                        log.error("Role " + roleName + " not found");
                         return new RuntimeException("Role " + roleName + " not found");
                     });
             rolesList.add(role);
         });
+
+        log.trace("Exiting getRolesAsRole method in UserServiceImpl");
         return rolesList;
     }
 }
