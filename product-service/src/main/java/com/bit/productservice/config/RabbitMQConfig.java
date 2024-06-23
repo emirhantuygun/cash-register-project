0087e16b346e69ff2acde15099dfb3d941d1087a
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-23 17:54:21 +0300

    add: JavaDoc for config package in product-service

--- product-service/src/main/java/com/bit/productservice/config/RabbitMQConfig.java
+++ product-service/src/main/java/com/bit/productservice/config/RabbitMQConfig.java
@@ -11,6 +11,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * This class is responsible for configuring RabbitMQ.
+ * It creates an exchange, a queue, and a binding between them.
+ * It also sets up a RabbitTemplate with a Jackson2JsonMessageConverter for message conversion.
+ *
+ * @author Emirhan Tuygun
+ */
 @Configuration
 public class RabbitMQConfig {
 
@@ -23,26 +30,60 @@ public class RabbitMQConfig {
     @Value("${rabbitmq.routingKey}")
     private String ROUTING_KEY;
 
+    /**
+     * This method creates a new instance of DirectExchange with the given exchange name.
+     * DirectExchange delivers messages to queues based on the routing key.
+     *
+     * @return a new instance of DirectExchange
+     */
     @Bean
     public DirectExchange exchange() {
         return new DirectExchange(EXCHANGE);
     }
 
+    /**
+     * This method creates a new instance of Queue with the given queue name.
+     * Queues are used to store messages that are consumed by applications.
+     *
+     * @return a new instance of Queue
+     */
     @Bean
     public Queue queue() {
         return new Queue(QUEUE, false);
     }
 
+    /**
+     * This method creates a new binding between the queue and the exchange.
+     * The binding is defined by the routing key, which determines which messages will be routed to the queue.
+     *
+     * @return a new instance of Binding
+     */
     @Bean
     public Binding binding() {
         return BindingBuilder.bind(queue()).to(exchange()).with(ROUTING_KEY);
     }
 
+    /**
+     * This method creates a new instance of Jackson2JsonMessageConverter.
+     * Jackson2JsonMessageConverter is a message converter provided by Spring AMQP that converts messages to and from JSON.
+     * It is used to serialize and deserialize messages sent and received over RabbitMQ.
+     *
+     * @return a new instance of Jackson2JsonMessageConverter
+     */
     @Bean
     public Jackson2JsonMessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
+    /**
+     * This method creates a new instance of RabbitTemplate.
+     * RabbitTemplate is a core component of Spring AMQP that provides a convenient API for sending and receiving messages.
+     * It uses a ConnectionFactory to establish connections to RabbitMQ and a MessageConverter to serialize and deserialize messages.
+     *
+     * @param connectionFactory the ConnectionFactory to establish connections to RabbitMQ
+     * @param jsonMessageConverter the Jackson2JsonMessageConverter to serialize and deserialize messages
+     * @return a new instance of RabbitTemplate
+     */
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter jsonMessageConverter) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
--- product-service/src/main/java/com/bit/productservice/config/RedisConfig.java
+++ product-service/src/main/java/com/bit/productservice/config/RedisConfig.java
@@ -7,9 +7,22 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+/**
+ * This class is responsible for configuring RedisTemplate for the application.
+ * RedisTemplate is a high-level abstraction over the Redis connection and provides
+ * convenient methods for interacting with Redis data structures.
+ *
+ * @author Emirhan Tuygun
+ */
 @Configuration
 public class RedisConfig {
 
+    /**
+     * This method creates and configures a RedisTemplate instance.
+     *
+     * @param connectionFactory The connection factory for Redis.
+     * @return A configured RedisTemplate instance.
+     */
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
