c69de2d9794b9854860be0118f8cfd7043453f3b
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-23 17:44:23 +0300

    add: JavaDoc for wrapper package in auth-service

--- auth-service/src/main/java/com/bit/authservice/wrapper/UpdateUserMessage.java
+++ auth-service/src/main/java/com/bit/authservice/wrapper/UpdateUserMessage.java
@@ -5,11 +5,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * This class represents a message for updating a user in the system.
+ * It contains the user's ID and the updated user information.
+ *
+ * @author Emirhan Tuygun
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateUserMessage {
+
     private Long id;
     private AuthUserRequest authUserRequest;
-
 }
