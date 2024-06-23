139b885628d01ef688924720a2a213905ba57abf
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-23 20:01:45 +0300

    add: JavaDoc for config package in sale-service

--- sale-service/src/main/java/com/bit/saleservice/config/RabbitMQConfig.java
+++ sale-service/src/main/java/com/bit/saleservice/config/RabbitMQConfig.java
@@ -11,6 +11,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * RabbitMQ's configuration class.
+ * This class is responsible for setting up RabbitMQ exchange, queue, binding, and message converter.
+ * It also provides a RabbitTemplate for sending and receiving messages.
+ *
+ * @author Emirhan Tuygun
+ */
 @Configuration
 public class RabbitMQConfig {
 
@@ -23,26 +30,53 @@ public class RabbitMQConfig {
     @Value("${rabbitmq.routingKey}")
     private String ROUTING_KEY;
 
+    /**
+     * Creates a DirectExchange instance with the given exchange name.
+     *
+     * @return the DirectExchange instance
+     */
     @Bean
     public DirectExchange exchange() {
         return new DirectExchange(EXCHANGE);
     }
 
+    /**
+     * Creates a Queue instance with the given queue name and durability.
+     *
+     * @return the Queue instance
+     */
     @Bean
     public Queue queue() {
         return new Queue(QUEUE, false);
     }
 
+    /**
+     * Creates a Binding instance that binds the queue to the exchange with the given routing key.
+     *
+     * @return the Binding instance
+     */
     @Bean
     public Binding binding() {
         return BindingBuilder.bind(queue()).to(exchange()).with(ROUTING_KEY);
     }
 
+    /**
+     * Creates a Jackson2JsonMessageConverter instance for converting messages to and from JSON.
+     *
+     * @return the Jackson2JsonMessageConverter instance
+     */
     @Bean
     public Jackson2JsonMessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
+    /**
+     * Creates a RabbitTemplate instance for sending and receiving messages.
+     *
+     * @param connectionFactory the RabbitMQ connection factory
+     * @param jsonMessageConverter the Jackson2JsonMessageConverter instance
+     * @return the RabbitTemplate instance
+     */
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter jsonMessageConverter) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
--- sale-service/src/main/java/com/bit/saleservice/config/RestTemplateConfig.java
+++ sale-service/src/main/java/com/bit/saleservice/config/RestTemplateConfig.java
@@ -4,8 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * This class is responsible for configuring a RestTemplate bean in the Spring application context.
+ * The RestTemplate is a convenient utility for making HTTP requests.
+ *
+ * @author Emirhan Tuygun
+ */
 @Configuration
 public class RestTemplateConfig {
+
+    /**
+     * This method creates and returns a new instance of RestTemplate.
+     * RestTemplate is a core Spring class for making HTTP requests.
+     *
+     * @return a new instance of RestTemplate
+     */
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
