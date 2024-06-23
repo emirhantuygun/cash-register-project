2d3a83e20d3a5c23eedbd0202e82d653e0f8af4f
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-23 18:14:23 +0300

    add: JavaDoc for repository package in product-service

--- product-service/src/main/java/com/bit/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/bit/productservice/repository/ProductRepository.java
@@ -10,26 +10,58 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
-import java.util.Optional;
 
+/**
+ * This interface represents the Product Repository, which extends the JpaRepository interface.
+ * It provides methods for interacting with the Product entity in the database.
+ *
+ * @author Emirhan Tuygun
+ */
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    /**
+     * This method retrieves all soft-deleted products from the database.
+     *
+     * @return a list of soft-deleted products
+     */
     @Query(value = "SELECT * FROM products WHERE deleted = true", nativeQuery = true)
     List<Product> findSoftDeletedProducts();
 
+    /**
+     * This method retrieves a paginated list of products based on the given specification and pageable.
+     *
+     * @param spec the specification for filtering and sorting the products
+     * @param pageable the pageable object for pagination
+     * @return a page of products
+     */
     Page<Product> findAll(Specification<Product> spec, Pageable pageable);
 
+    /**
+     * This method checks if a product with the given id exists and is soft-deleted.
+     *
+     * @param id the id of the product
+     * @return true if the product exists and is soft-deleted, false otherwise
+     */
     @Query(value = "SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END FROM products p WHERE p.id = :id AND p.deleted = true", nativeQuery = true)
     boolean existsByIdAndDeletedTrue(@Param("id") Long id);
 
+    /**
+     * This method restores a soft-deleted product by setting its deleted flag to false.
+     *
+     * @param id the id of the product to be restored
+     */
     @Transactional
     @Modifying
     @Query(value = "UPDATE products SET deleted = false WHERE id = :id", nativeQuery = true)
     void restoreProduct(@Param("id") Long id);
 
+    /**
+     * This method permanently deletes a product from the database.
+     *
+     * @param id the id of the product to be deleted permanently
+     */
     @Transactional
     @Modifying
     @Query(value = "DELETE FROM products WHERE id = :id", nativeQuery = true)
