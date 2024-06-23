e0a10a07511ae6afc9d9bb21e8db3bca78c434fd
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-23 19:58:30 +0300

    add: JavaDoc for annotation package in report-service

--- sale-service/src/main/java/com/bit/saleservice/annotation/ExcludeFromGeneratedCoverage.java
+++ sale-service/src/main/java/com/bit/saleservice/annotation/ExcludeFromGeneratedCoverage.java
@@ -5,6 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is used to mark methods that should be excluded from code coverage reports.
+ * It is typically used in testing frameworks or code generators to ensure that certain methods
+ * are not included in the generated coverage reports.
+ *
+ * @author Emirhan Tuygun
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface ExcludeFromGeneratedCoverage {
