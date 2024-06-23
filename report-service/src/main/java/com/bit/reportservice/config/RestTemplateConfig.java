1767db560be5b7ff86f2666eda3287b02420e6a8
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-23 19:35:38 +0300

    add: JavaDoc for config package in report-service

--- report-service/src/main/java/com/bit/reportservice/config/RestTemplateConfig.java
+++ report-service/src/main/java/com/bit/reportservice/config/RestTemplateConfig.java
@@ -9,9 +9,20 @@ import org.springframework.http.converter.HttpMessageConverter;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Configuration class for RestTemplate.
+ * This class provides a RestTemplate bean with custom message converters.
+ *
+ * @author Emirhan Tuygun
+ */
 @Configuration
 public class RestTemplateConfig {
+
+    /**
+     * Creates a RestTemplate bean with custom message converters.
+     *
+     * @return the configured RestTemplate bean
+     */
     @Bean
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
@@ -19,6 +30,12 @@ public class RestTemplateConfig {
         return restTemplate;
     }
 
+    /**
+     * Returns a list of HttpMessageConverter objects for customizing RestTemplate.
+     * In this case, it adds MappingJackson2HttpMessageConverter for JSON support.
+     *
+     * @return a list of HttpMessageConverter objects
+     */
     private List<HttpMessageConverter<?>> getMessageConverters() {
         List<HttpMessageConverter<?>> converters = new ArrayList<>();
         converters.add(new MappingJackson2HttpMessageConverter());
