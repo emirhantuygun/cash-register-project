60b75612c6630ce6184ce042ff1efb040509691d
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-23 23:23:47 +0300

    add: JavaDoc for config package in user-service

--- user-management-service/src/main/java/com/bit/usermanagementservice/config/RabbitMQConfig.java
+++ user-management-service/src/main/java/com/bit/usermanagementservice/config/RabbitMQConfig.java
@@ -11,6 +11,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * This class is responsible for configuring RabbitMQ.
+ * It includes the creation of exchanges, queues, bindings, and a RabbitTemplate.
+ *
+ * @author Emirhan Tuygun
+ */
 @Configuration
 public class RabbitMQConfig {
 
@@ -47,66 +53,152 @@ public class RabbitMQConfig {
     @Value("${rabbitmq.routingKey.restore}")
     private String ROUTING_KEY_RESTORE;
 
+    /**
+     * This method creates a new instance of DirectExchange.
+     * DirectExchange delivers messages to queues based on the routing key.
+     *
+     * @return a new instance of DirectExchange
+     */
     @Bean
     public DirectExchange userExchange() {
         return new DirectExchange(EXCHANGE);
     }
 
+    /**
+     * This method creates a new instance of Queue for user creation events.
+     * The queue is bound to the userExchange with the routing key for user creation events.
+     *
+     * @return a new instance of Queue for user creation events
+     */
     @Bean
     public Queue userQueueCreate() {
         return new Queue(USER_QUEUE_CREATE, false);
     }
 
+    /**
+     * This method creates a new instance of Queue for user update events.
+     * The queue is bound to the userExchange with the routing key for user update events.
+     *
+     * @return a new instance of Queue for user update events
+     */
     @Bean
     public Queue userQueueUpdate() {
         return new Queue(USER_QUEUE_UPDATE, false);
     }
 
+    /**
+     * This method creates a new instance of Queue for user delete events.
+     * The queue is bound to the userExchange with the routing key for user delete events.
+     *
+     * @return a new instance of Queue for user delete events
+     */
     @Bean
     public Queue userQueueDelete() {
         return new Queue(USER_QUEUE_DELETE, false);
     }
 
+    /**
+     * This method creates a new instance of Queue for user delete permanent events.
+     * The queue is bound to the userExchange with the routing key for user delete permanent events.
+     *
+     * @return a new instance of Queue for user delete permanent events
+     */
     @Bean
     public Queue userQueueDeletePermanent() {
         return new Queue(USER_QUEUE_DELETE_PERMANENT, false);
     }
 
+    /**
+     * This method creates a new instance of Queue for user restore events.
+     * The queue is bound to the userExchange with the routing key for user restore events.
+     *
+     * @return a new instance of Queue for user restore events
+     */
     @Bean
     public Queue userQueueRestore() {
         return new Queue(USER_QUEUE_RESTORE, false);
     }
 
+    /**
+     * This method creates a new instance of Binding for user creation events.
+     * A Binding is a relationship between an exchange and a queue,
+     * and it specifies the routing key that should be used when sending messages to the queue.
+     *
+     * @return a new instance of Binding for user creation events
+     */
     @Bean
     public Binding bindingCreate() {
         return BindingBuilder.bind(userQueueCreate()).to(userExchange()).with(ROUTING_KEY_CREATE);
     }
 
+    /**
+     * This method creates a new instance of Binding for user update events.
+     * A Binding is a relationship between an exchange and a queue,
+     * and it specifies the routing key that should be used when sending messages to the queue.
+     *
+     * @return a new instance of Binding for user update events
+     */
     @Bean
     public Binding bindingUpdate() {
         return BindingBuilder.bind(userQueueUpdate()).to(userExchange()).with(ROUTING_KEY_UPDATE);
     }
 
+    /**
+     * This method creates a new instance of Binding for user delete events.
+     * A Binding is a relationship between an exchange and a queue,
+     * and it specifies the routing key that should be used when sending messages to the queue.
+     *
+     * @return a new instance of Binding for user delete events
+     */
     @Bean
     public Binding bindingDelete() {
         return BindingBuilder.bind(userQueueDelete()).to(userExchange()).with(ROUTING_KEY_DELETE);
     }
 
+    /**
+     * This method creates a new instance of Binding for user delete permanent events.
+     * A Binding is a relationship between an exchange and a queue,
+     * and it specifies the routing key that should be used when sending messages to the queue.
+     *
+     * @return a new instance of Binding for user delete permanent events
+     */
     @Bean
     public Binding bindingDeletePermanent() {
         return BindingBuilder.bind(userQueueDeletePermanent()).to(userExchange()).with(ROUTING_KEY_DELETE_PERMANENT);
     }
 
+    /**
+     * This method creates a new instance of Binding for user restore events.
+     * A Binding is a relationship between an exchange and a queue,
+     * and it specifies the routing key that should be used when sending messages to the queue.
+     *
+     * @return a new instance of Binding for user restore events
+     */
     @Bean
     public Binding bindingRestore() {
         return BindingBuilder.bind(userQueueRestore()).to(userExchange()).with(ROUTING_KEY_RESTORE);
     }
 
+    /**
+     * This method creates a new instance of Jackson2JsonMessageConverter.
+     * Jackson2JsonMessageConverter is a Spring AMQP message converter that uses the Jackson library to convert messages to and from JSON.
+     *
+     * @return a new instance of Jackson2JsonMessageConverter
+     */
     @Bean
     public Jackson2JsonMessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
+    /**
+     * This method creates a new instance of RabbitTemplate.
+     * RabbitTemplate is a high-level abstraction for sending and receiving messages.
+     * It uses a ConnectionFactory to establish connections to the RabbitMQ server.
+     *
+     * @param connectionFactory the ConnectionFactory to use for establishing connections
+     * @param jsonMessageConverter the Jackson2JsonMessageConverter to use for converting messages to and from JSON
+     * @return a new instance of RabbitTemplate
+     */
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter jsonMessageConverter) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
