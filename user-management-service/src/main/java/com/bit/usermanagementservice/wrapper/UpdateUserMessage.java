6211e5499c7a5e7051137807cdc9575521d9f443
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2024-06-24 00:41:27 +0300

    add: JavaDoc for wrapper package in user-service

--- user-management-service/src/main/java/com/bit/usermanagementservice/wrapper/UpdateUserMessage.java
+++ user-management-service/src/main/java/com/bit/usermanagementservice/wrapper/UpdateUserMessage.java
@@ -5,11 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * This class represents a message for updating a user.
+ * It contains the user's ID and the updated user information.
+ *
+ * @author Emirhan Tuygun
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateUserMessage {
     private Long id;
     private AuthUserRequest authUserRequest;
-
 }
